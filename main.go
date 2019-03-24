d23c4dc93701f1dcc96ee9b347fe623a0faf4b02
majid <majidbigdeli72@gmail.com>
2019-03-24 05:45:06 +0430

    — Reset

--- main.go
+++ main.go
@@ -4,16 +4,24 @@ import (
 	"SimpleProject/Domin/data"
 	"SimpleProject/Domin/util"
 	"SimpleProject/UI/controller"
+	"fmt"
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/kardianos/minwinsvc"
 	"github.com/kataras/iris"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")    // optionally look for config in the working directory
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
 	data.GetDB()
+
 	app := iris.Default()
 	app.Use(util.Cors)
-
 	//AccountController
 	accountController := app.Party("/api/Account")
 	accountController.Post("/Login", controller.Login)
