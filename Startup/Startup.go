2eb8599fbc985af2bae7bc8dd64a6209e39b8342
majid <majidbigdeli72@gmail.com>
2019-03-24 07:09:34 +0430

    — Fix Windows Service Issue For Exe File Path

--- Startup/Startup.go
+++ Startup/Startup.go
@@ -3,16 +3,26 @@ package Startup
 import (
 	"SimpleProject/Domin/data"
 	"fmt"
+
+	"github.com/kardianos/osext"
+
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/kardianos/minwinsvc"
 	"github.com/spf13/viper"
 )
 
 func init() {
+	path, err := osext.ExecutableFolder()
+
+	if err != nil {
+		panic(fmt.Errorf("Fatal error ExecutableFolder: %s \n", err))
+
+	}
+
 	viper.SetConfigName("config")
-	viper.AddConfigPath(".")    // optionally look for config in the working directory
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil {             // Handle errors reading the config file
+	viper.AddConfigPath(path)  // optionally look for config in the working directory
+	err = viper.ReadInConfig() // Find and read the config file
+	if err != nil {            // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 	data.GetDB()
