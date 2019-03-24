29b0bbdc05227705669ef62966858fed552a3ea7
majid <majidbigdeli72@gmail.com>
2019-03-24 05:47:48 +0430

    — Reset

---
+++ Domin/Data/db.go
@@ -0,0 +1,44 @@
+package data
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
+)
+
+type dbConfig struct {
+	Debug    bool   `mapstructure:"debug"`
+	Password string `mapstructure:"password"`
+	Port     int    `mapstructure:"port"`
+	Server   string `mapstructure:"server"`
+	User     string `mapstructure:"user"`
+	Database string `mapstructure:"database"`
+}
+
+var db *sqlx.DB
+
+//GetDB ...
+func GetDB() {
+
+	var con dbConfig
+
+	if err := viper.UnmarshalKey("db", &con); err != nil {
+		panic(err)
+	}
+
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;", con.Server, con.User, con.Password, con.Port, con.Database)
+
+	if con.Debug {
+		fmt.Printf(" connString:%s\n", connString)
+	}
+	conn, err := sqlx.Connect("sqlserver", connString)
+	if err != nil {
+		//log.Fatal("Open connection failed:", err.Error())
+		panic(err)
+	}
+
+	//	fmt.Printf("Connected!\n")
+	//	defer conn.Close()
+	db = conn
+	//return conn
+}
